1661f05f1135d1b89fd2b595d1dcd6a5503bf42d
Bruno Muniz <brunoamuniz@gmail.com>
2019-01-18 17:20:53 -0300

    Ajustes no splash

---
+++ src/main/java/br/com/totalcross/sample/nubank/Nubank.java
@@ -0,0 +1,32 @@
+package br.com.totalcross.sample.nubank;
+
+import br.com.totalcross.sample.nubank.ui.Inicial;
+import br.com.totalcross.sample.nubank.ui.SplashWindow;
+import totalcross.io.IOException;
+import totalcross.sys.Settings;
+import totalcross.ui.MainWindow;
+import totalcross.ui.image.ImageException;
+
+public class Nubank extends MainWindow{
+	public Nubank(){
+		setUIStyle(Settings.Material);
+		Settings.uiAdjustmentsBasedOnFontHeight = true;
+	}
+	
+	public void initUI(){
+		SplashWindow sp;
+		Inicial inicial = new Inicial();
+		try {
+			sp = new SplashWindow();
+			sp.popupNonBlocking();
+			swap(inicial);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ImageException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/br/com/totalcross/sample/nubank/ui/SplashWindow.java
@@ -0,0 +1,53 @@
+package br.com.totalcross.sample.nubank.ui;
+
+import totalcross.io.IOException;
+import totalcross.ui.ImageControl;
+import totalcross.ui.Window;
+import totalcross.ui.anim.ControlAnimation;
+import totalcross.ui.anim.FadeAnimation;
+import totalcross.ui.gfx.Color;
+import totalcross.ui.image.Image;
+import totalcross.ui.image.ImageException;
+
+public class SplashWindow extends Window {
+
+	public SplashWindow() throws IOException, ImageException {
+
+	}
+
+	protected void onPopup() {
+		ImageControl logo, back;
+		try {
+			back = new ImageControl(new Image("images/ic_adaptive_launcher_shell_background.png"));
+			back.scaleToFit = true;
+			back.centerImage = true;
+			back.hwScale = true;
+			back.strechImage = true;
+			add(back, LEFT, TOP, FILL, FILL);
+
+			// setBackColor(Color.getRGB(136, 47, 146));
+			logo = new ImageControl(new Image("images/ic_adaptive_launcher_shell_round.png"));
+			logo.scaleToFit = true;
+			logo.centerImage = true;
+			logo.transparentBackground = true;
+			// logo.hwScale = true;
+			// logo.strechImage = true;
+			add(logo, CENTER, CENTER, PARENTSIZE + 50, PARENTSIZE + 50);
+
+			FadeAnimation.create(logo, true, null, 3000)
+					.then(FadeAnimation.create(logo, false, this::onAnimationFinished, 3000)).start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ImageException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void onAnimationFinished(ControlAnimation anim) {
+		this.unpop();
+	}
+
+}
