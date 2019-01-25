3782d372a6323edf19cb0510eb1522d0e1dc77f5
João Pedro <joanpedoro6@gmail.com>
2019-01-25 09:47:09 -0300

    Adc cor do scroll na class Colors e apagando o pacote duplicado util

--- src/main/java/br/com/totalcross/sample/nubank/util/Colors.java
+++ src/main/java/br/com/totalcross/sample/nubank/util/Colors.java
@@ -5,4 +5,5 @@ public class Colors {
 	public static final int PURPLE_BUTTONS = 0X6CA175;
 	public static final int BLACK = 0x000000;
 	public static final int BACKGROUND = 0x88925C;
+	public static final int SCROLL = 0xa140b3;
 }
--- src/main/java/util/Colors.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-public class Colors {
-	public static final int WHITE = 0xFFFFFF;
-	public static final int PURPLE_BUTTONS = 0X6CA175;
-	public static final int BLACK = 0x000000;
-	public static final int BACKGROUND = 0x88925C;
-}
